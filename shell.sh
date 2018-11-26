1614dfc28a1573fd866fcd1d357da9a9b9f19304
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2018-11-26 15:50:50 -0300

    Modify generated log

--- shell.sh
+++ shell.sh
@@ -5,6 +5,14 @@ bold=`tput bold`;
 reset=`tput sgr0`;
 blue=`tput setaf 33`;
 
+if [ ! -e ~/accounting.txt ] ; then
+    echo "ID REAl_TIME USER_TIME SYS_TIME" > ~/accounting.txt;
+fi
+
+if [ ! -e ~/command_history ] ; then
+    echo "ID DATE COMMAND" > ~/command_history;
+fi
+
 clear;
 cd;
 
@@ -16,16 +24,30 @@ function short_dir {
     echo $dir;
 }
 
+
+function get_id {
+    last_id=$(tail -n 1 ~/accounting.txt | awk '{print $1}');
+
+    if ! [[ $last_id =~ ^[0-9]+$ ]] ; then
+        last_id=0;
+    fi
+
+    id=$(expr 1 + $last_id);
+    echo $id;
+}
+
 while read -p "${red}${bold}$USER"'@ll-best-shell'"${reset}:${blue}${bold}$(short_dir)${reset}"'$ ' -ea line;
 do
+
+  id=$(get_id);
 
   if [[ "${line[0]}" = "cd" ]] || [[ "${line[0]}" = "exit" ]] ; then
       ${line[@]};
   else
-      echo "${line[@]}" | time -ao ~/accounting.txt -f "%E real, %U user, %S sys" bash; 
+      echo "${line[@]}" | /usr/bin/time -ao ~/accounting.txt -f "$id %E %U %S" bash; 
   fi;
 
-  echo "${line[@]}" >> ~/command_history
+  echo "$id $(date +\%F) ${line[0]}" >> ~/command_history
 
 done < "${1:-/dev/stdin}";
 echo
